5bddcc38c53df725d1493745135fb21b825c9a6d
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-25 16:01:50 -0700

    Set up initial files for header, app components and render header to page

---
+++ client/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <header className="row">
+      <nav className="navbar navbar-expand-xl navbar-dark bg-dark col">
+        <a className="navbar-brand" href="/">$ Wicked Sales</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
