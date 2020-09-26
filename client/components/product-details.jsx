d0d1ab444f486edb34c1c78d622bcf09fafab2be
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-26 11:28:29 -0700

    Set up product detail component

---
+++ client/components/product-details.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row"></div>
+      </div>
+    );
+  }
+}
+
+export default ProductDetails;
