59e799202256d8791c52953eb65e4428126e5c72
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-26 12:42:01 -0700

    Edited static render to populate with prelimiary data fetch

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -11,47 +11,41 @@ class ProductDetails extends React.Component {
   componentDidMount() {
     fetch('/api/products/1')
       .then(res => res.json())
-      .then(product => {
-        this.setState({
-          product: product
-        });
-      })
+      .then(product => this.setState({
+        product: product
+      }))
       .catch(err => console.error(err));
   }
 
   render() {
-    return (
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col">
-            <a>Back to catalog</a>
+    if (this.state.product) {
+      return (
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col mb-5">
+              <a >Back to catalog</a>
+            </div>
+          </div>
+          <div className="row mb-5">
+            <div className="col-6">
+              <img src={this.state.product.image} alt="" />
+            </div>
+            <div className="col-6">
+              <h1 className="mb-3">{this.state.product.name}</h1>
+              <h3 className="mb-3">{this.state.product.price}</h3>
+              <p>{this.state.product.shortDescription}</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <p>{this.state.product.longDescription}</p>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-6">
-            <img src="server\public\images\shake-weight.jpg" alt=""/>
-          </div>
-          <div className="col-6">
-            <h1 className="mb-3">Name</h1>
-            <h3 className="mb-3">$0.00</h3>
-            <p>Product short description: Lorem ipsum dolor sit, amet consectetur
-              adipisicing elit. Adipisci suscipit provident delectus voluptatibus
-              perspiciatis aliquid consectetur. Molestiae?</p>
-          </div>
-        </div>
-        <div className="row mt-4">
-          <div className="col">
-            <p>Product long description: Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Ipsam ipsum reprehenderit illo ipsa provident iure
-              repellendus perferendis facere voluptatem, eveniet doloribus excepturi
-              consequuntur quam perspiciatis architecto, veniam cupiditate labore
-              distinctio laudantium aperiam aliquid. Magni, non ad? Lorem ipsum dolor
-              sit amet consectetur adipisicing elit. Sequi, cumque praesentium
-              velit quod debitis illo est similique cupiditate laudantium. Facilis?</p>
-          </div>
-        </div>
-      </div>
-    );
+      );
+    } else {
+      return null;
+    }
   }
 }
 
